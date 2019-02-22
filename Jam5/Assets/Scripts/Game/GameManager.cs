445e08e99d1b3a041ae1903ea38c6b25772a32fa
Herman McElveen (RIT Student) <hxm1417@ad.rit.edu>
2019-02-22 14:46:05 -0500

    Starting camera movement

---
+++ Jam5/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private GameObject cam;
+    private Vector3 position;
+    [SerializeField] private float speed;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main.gameObject;
+        position = cam.transform.position;
+        speed = 2.0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        position.y += speed * Time.deltaTime;
+
+        cam.transform.position = position;
+	}
+}
