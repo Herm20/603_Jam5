f4395f0fc3bd5d91ca61993ef2bebf9658e131a7
JaJuan Webster (RIT Student) <jxw7456@ad.rit.edu>
2019-02-24 17:33:44 -0500

    merge complete

---
+++ Jam5/Assets/Scripts/Game/FlashingText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Shows string and then hides it with time step
+public class FlashingText : MonoBehaviour
+{
+    // Attributes
+    [SerializeField] private Text textToUse;
+    [SerializeField] private bool useText;
+    [SerializeField] private string flashingString;
+    [SerializeField] private float pauseTime;
+
+    // Initialize
+    private void Start()
+    {
+        if (useText)
+        {
+            textToUse = GetComponent<Text>();
+            flashingString = textToUse.text;
+        }            
+
+        textToUse.text = "";
+
+        StartCoroutine(TypeText(textToUse, flashingString, pauseTime));
+    }
+
+    // Writes out text after waiting for the timeout
+    private IEnumerator TypeText(Text text, string stringToUse, float timePause)
+    {
+        bool show = true;
+        while (true)
+        {
+            if (show)
+                textToUse.text = stringToUse;
+            else
+                textToUse.text = "";
+
+            show = !show;
+            yield return 0;
+            yield return new WaitForSeconds(timePause);
+        }
+    }
+}
