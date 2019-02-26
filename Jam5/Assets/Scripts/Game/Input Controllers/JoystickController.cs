395fa081ba2db75f29c7d5f44e5ff4bde2c3465d
Herman McElveen (RIT Student) <hxm1417@ad.rit.edu>
2019-02-26 11:23:23 -0500

    Adding changes to menu scenes

--- Jam5/Assets/Scripts/Game/Input Controllers/JoystickController.cs
+++ Jam5/Assets/Scripts/Game/Input Controllers/JoystickController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class JoystickController : MonoBehaviour {
     public PlayerController playerController;
     public PlayerSpawnData playerData;
+    public PlayerManager playerManager;
 
     [SerializeField] float jumpPower = 5f;
 
     public InputData inputData;
 
+    private Scene currScene;
+
     // Use this for initialization
     void Awake () {
         playerController = GetComponent<PlayerController>();
+        playerManager = GameObject.Find("GameManager").GetComponent<PlayerManager>();
+        currScene = SceneManager.GetActiveScene();
 	}
 	
 	// Update is called once per frame
@@ -31,29 +37,21 @@ public class JoystickController : MonoBehaviour {
             playerController.arrow.gameObject.SetActive(true);
             playerController.arrow.up = inputDirection;
         }
-        
-        if (Input.GetButtonDown(inputData.aButton))
-            playerController.Jump(inputDirection, jumpPower);
 
-        if (Input.GetButtonDown(inputData.bButton))
-            playerController.Use(inputDirection);
-    }
-
-    void playerInput(int index) {
-        if (Input.GetButtonDown(playerData.playerInput.controllers[index].aButton))
+        if (Input.GetButtonDown(inputData.aButton) && currScene.name == "Game")
         {
-            Vector3 inputDirection = Vector3.zero;
-            inputDirection.x = Input.GetAxis(playerData.playerInput.controllers[index].horizontalAxis);
-            inputDirection.y = Input.GetAxis(playerData.playerInput.controllers[index].verticalAxis);
             playerController.Jump(inputDirection, jumpPower);
         }
 
-        if (Input.GetButtonDown(playerData.playerInput.controllers[index].bButton))
+        if (Input.GetButtonDown(inputData.bButton) && currScene.name == "Game")
         {
-            Vector3 inputDirection = Vector3.zero;
-            inputDirection.x = Input.GetAxis(playerData.playerInput.controllers[index].horizontalAxis);
-            inputDirection.y = Input.GetAxis(playerData.playerInput.controllers[index].verticalAxis);
             playerController.Use(inputDirection);
         }
+        else if (Input.GetButtonDown(inputData.bButton) && currScene.name == "SelectMenu")
+        {
+            playerManager.selectionGrids[inputData.controllerNum - 1].color = Color.red;
+            playerManager.readyUp[inputData.controllerNum - 1] = true;
+        }
+
     }
 }
